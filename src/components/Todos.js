628b9d4d798cad6f542569eff1890621b0473475
Christian Pari <ChristianPari@outlook.com>
2020-07-29 20:40:19 -0400

    Create, delete, mark complete all work and basic "todo list" is achieved w/ JSON Placeholder API data

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 
-class Todos extends Component { 
+class Todos extends Component {
 
     render() {
         // using the map method to serve as a for loop to then access and display the desired data from the TodoItem component which gets it's info from the Todos property which gathered it's info from the state object todos property of the main App component
 
-        return this.props.todos.map((todo) => (
-                <TodoItem key={ todo.id } todo={ todo } markComplete={ this.props.markComplete } delTodo={ this.props.delTodo} />
+        return this.props.todos.map((todo) => ( <
+            TodoItem key = { todo.id }
+            todo = { todo }
+            markComplete = { this.props.markComplete }
+            delTodo = { this.props.delTodo }
+            />
         ));
     }
 }
 
 // PropTypes
 Todos.propTypes = {
-    todos: PropTypes.array.isRequired
+    todos: PropTypes.array.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
 }
 
 export default Todos;
